66d0a56621b52996efd88671d2b54c2de59b55ee
Ahmed Aboelgheit <am227450@gmail.com>
2026-08-10 21:27:19 +0300

    ready

--- playwright.config.js
+++ playwright.config.js
@@ -1,16 +1,27 @@
-const { defineConfig } = require('@playwright/test');
+const { defineConfig, devices } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+
+  // Limit execution strictly to Desktop Chrome
+  projects: [
+    {
+      name: 'chromium',
+      use: { ...devices['Desktop Chrome'] },
+    },
+  ],
+
   reporter: [
     ['html', { outputFolder: 'playwright-report', open: 'never' }],
     ['list']
   ],
+
   use: {
     headless: true,
+
     /* Collect trace when retrying a failed test */
     trace: 'on-first-retry',
-    
+
     /* Take screenshots automatically on failure */
     screenshot: 'only-on-failure',
   },
