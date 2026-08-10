50b8aa3eb13df6322aee7c8849b50dff043e2043
Ahmed Aboelgheit <am227450@gmail.com>
2026-08-10 21:16:00 +0300

    ready

--- playwright.config.js
+++ playwright.config.js
@@ -2,7 +2,16 @@ const { defineConfig } = require('@playwright/test');
 
 module.exports = defineConfig({
   testDir: './tests',
+  reporter: [
+    ['html', { outputFolder: 'playwright-report', open: 'never' }],
+    ['list']
+  ],
   use: {
-    headless: true
+    headless: true,
+    /* Collect trace when retrying a failed test */
+    trace: 'on-first-retry',
+    
+    /* Take screenshots automatically on failure */
+    screenshot: 'only-on-failure',
   },
 });
